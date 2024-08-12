94f137d653d1f2aeefc8c374068112d38c2d2444
Mac Studio <macstudio@macstudio.local>
2024-08-12 16:24:20 +0800

    fix: simplify routers

--- router.go
+++ router.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 
-	"github.com/rainbow-io-llc/tv-bot/alpaca"
+	"github.com/rainbow-io-llc/tv-bot/handlers"
 )
 
 func register(r *gin.Engine) {
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-		  "message": "pong",
-		})
-	  })
-
-	r.POST("/webhook", func(c *gin.Context) {
-		assets, err := alpaca.Trader.Check("AAPL")	
-		if err != nil {
-			log.Printf("\n[ALPACA-error] failed to check orders: %s\n", err)
-		} 
-		if assets != nil {
-			log.Printf("\n[ALPACA-info] orders: %+v\n", assets)	
-		}
-	})
+	r.GET("/ping", handlers.Ping())
+	r.POST("/webhook", handlers.Webhook())
 }
